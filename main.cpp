5d30684b72a429ed49586b9c93f928c3e94a31d0
badzu <33403548+badzu@users.noreply.github.com>
2018-05-21 22:58:50 +0200

    1.3.2
    Działająca wersja programu, która wprowadza jedynie kilka kosmetycznych zmian. Zostały usunięte nie potrzebne biblioteki i została poprawiona czytelność kodu.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
-#include <conio.h>
 #include <fstream>
 #include <string>
 #include <windows.h>
-#include <cstdlib>
-#include<sstream>
+#include <sstream>
 using namespace std;
 
-class ParaX
+class Paragraphx
 {
     public:
-    string sytuacja[5];
-    string tresc[5];
-    string odpA[5];
-    string odpB[5];
-    string odpC[5];
-    string odpD[5];
-	string idA[5]; int idxA[5];
-	string idB[5]; int idxB[5];
-	string idC[5]; int idxC[5];
-	string idD[5]; int idxD[5];
-
+    string sytuacja[100];
+    string tresc[100];
+    string odpA[100];
+    string odpB[100];
+    string odpC[100];
+    string odpD[100];
+	string idA[100];
+	string idB[100];
+	string idC[100];
+	string idD[100];
 	string wybor="0";
 
-    int wczytywanie()
+void wczytywanie()
 {
     int nr_linii=1;
     int pytanie=0;
     string linia;
-    int wartosc;
 
     fstream plik;
     plik.open("data.txt",ios::in);
@@ -43,33 +39,32 @@ class ParaX
     {
         switch(nr_linii)
         {
-        	case 1: sytuacja[pytanie]=linia; 			   break;
-            case 2: tresc[pytanie] = linia;         break;
-            case 3: idA[pytanie] = linia;        break;
-            case 4: odpA[pytanie] = linia;        break;
-            case 5: idB[pytanie] = linia;        break;
-            case 6: odpB[pytanie] = linia;        break;
-            case 7: idC[pytanie] = linia;        break;
-            case 8: odpC[pytanie] = linia;        break;
-            case 9: idD[pytanie] = linia;        break;
+        	case 1: sytuacja[pytanie]=linia; 	   break;
+            case 2: tresc[pytanie] = linia;        break;
+            case 3: idA[pytanie] = linia;          break;
+            case 4: odpA[pytanie] = linia;         break;
+            case 5: idB[pytanie] = linia;          break;
+            case 6: odpB[pytanie] = linia;         break;
+            case 7: idC[pytanie] = linia;          break;
+            case 8: odpC[pytanie] = linia;         break;
+            case 9: idD[pytanie] = linia;          break;
             case 10: odpD[pytanie] = linia;        break;
-            case 11: break;
+            case 11:                               break;
         }
 
         if (nr_linii==11) {nr_linii=0;pytanie++;}
         nr_linii++;
     }
-
     plik.close();
-
 	}
 
-	int control()
-	{
+void logika()
+{
+	int X=0;
 
-	int i, X=0;
     while(true)
     {
+            system("CLS");
             if(wybor==odpA[X]){wybor=idA[X];}
             if(wybor==odpB[X]){wybor=idB[X];}
             if(wybor==odpC[X]){wybor=idC[X];}
@@ -77,27 +72,37 @@ class ParaX
             istringstream iss(wybor);
             iss>> X;
             if(wybor==sytuacja[X])
+            {
+                int k=0;
+                while(true)
                 {
-
-                    cout<<tresc[X]<<endl;
-                    cout<<odpA[X]<<endl;
-                    cout<<odpB[X]<<endl;
-                    cout<<odpC[X]<<endl;
-                    cout<<odpD[X]<<endl<<endl;
-                    cin>>wybor;
-
+                    Sleep(20);
+                cout<<tresc[X].at(k);
+                    k++;
+                    if(tresc[X].at(k)=='$') { Sleep(200); cout<<endl<<endl; break; }
                 }
+                cout<<odpA[X]<<endl;
+                cout<<odpB[X]<<endl;
+                cout<<odpC[X]<<endl;
+                cout<<odpD[X]<<endl<<endl;
+                cout<<"Wybor interakcji : ";
+                cin>>wybor;
+
+            }
+            else
+            {
+                cout<<"GAME OVER";
+                break;
+            }
     }
-
 }
-};
 
+};
 
 int main()
 {
-  ParaX Tekst;
-  Tekst.wczytywanie();
-  Tekst.control();
-
+  Paragraphx Gra;
+  Gra.wczytywanie();
+  Gra.logika();
 }
 
